73a1065ad42d87d6ff503483b90461e8a34d803d
tarotaro <80757996+tarotao@users.noreply.github.com>
2021-06-23 21:39:25 +0900

    bord

--- boyo-jhs/src/App.vue
+++ boyo-jhs/src/App.vue
@@ -1,17 +1,76 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>送信フォーム</h3>
+    <label for="name">名前：</label>
+    <input 
+      v-model="name" 
+      id="name" 
+      type="text"
+    >
+    <br><br>
+    <label for="comment">コメント：</label>
+    <textarea  
+      v-model="comment" 
+      id="comment"
+    ></textarea>
+    <br><br>
+    <button @click="createComment">送信</button>
+    <h2>掲示板</h2>
+    <div v-for="post in posts" :key="post.name">
+      <div>名前：{{post.fields.name.stringValue}}</div>
+      <div>{{post.fields.comment.stringValue}}</div>
+      <hr>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      name: "",
+      comment: "",
+      posts: []
+    }
+  },
+  created() {
+    axios
+      .get(
+        '/comments'
+      )
+      .then(res => {
+        this.posts = res.data.documents;
+        console.log(res);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  },
+  methods: {
+    createComment() {
+      axios.post(
+        '/comments',
+        {
+          fields: {
+            name: {
+              stringValue: this.name
+            },
+            comment: {
+              stringValue: this.comment
+            }
+          }
+        }
+      )
+      .then(res => {
+        console.log(res);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+     this.name =''
+     this.comment = ''
+    }
   }
 }
 </script>
--- boyo-jhs/src/main.js
+++ boyo-jhs/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import axios from "axios"
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = "https://firestore.googleapis.com/v1/projects/boyo-bord/databases/(default)/documents"
 
 new Vue({
   render: h => h(App),
